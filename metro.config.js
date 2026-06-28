874da6dece5b26e4c92939ba3fd88905c3553f5d
Michael Wu <quantumSimplx@gmail.com>
2026-06-27 18:02:06 -0700

    fix(web): add wasm metro support + cross-origin headers for expo-sqlite on web

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,9 @@ const { getDefaultConfig } = require('@expo/metro-config')
 const { withNativeWind } = require('nativewind/metro')
 
 const config = getDefaultConfig(__dirname)
+
+// expo-sqlite on web requires wasm bundling support
+config.resolver.assetExts.push('wasm')
+config.resolver.sourceExts = config.resolver.sourceExts.filter(ext => ext !== 'wasm')
+
 module.exports = withNativeWind(config, { input: './src/global.css' })
